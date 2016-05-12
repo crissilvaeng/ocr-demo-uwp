9055e59c552adb4ae4418f9d8a90068dd6be85f7
CristinaSilvaEng <cristina.silva@openmailbox.org>
2016-05-12 03:00:38 -0300

    :rocket: Carrega imagem do storage para a tela.

---
+++ Ocr.Demo.Core/OcrManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Ocr.Demo.Core
+{
+    /// <summary>
+    /// Classe de entrada do Core. Fornece acesso funções por ele implementadas.
+    /// </summary>
+    public class OcrManager
+    {
+        // Public methods
+        /// <summary>
+        /// Resulta em um StorageFile de uma imagem.
+        /// </summary>
+        /// <returns>Tarefa cujo resultado é um StorageFile.</returns>
+        public async Task<StorageFile> GetImageFromStorage()
+        {
+            // Cria instância de picker para documentos no storage.
+            FileOpenPicker picker = new FileOpenPicker();
+            // Exibe os documentos em thumbnail.
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            // Inicia na pasta de imagens.
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            // Filtra para extensões .jpg, .jpeg e .png.
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            // Aguarda retorno da escolha de um arquivo.
+            StorageFile file = await picker.PickSingleFileAsync();
+
+            // Devolve storage file para a chamada do método.
+            return file;
+        }
+    }
+}
--- Ocr.Demo.Ui/MainPage.xaml.cs
+++ Ocr.Demo.Ui/MainPage.xaml.cs
@@ -1,30 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Ocr.Demo.Core;
+using System;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Ocr.Demo.Ui
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Página principal da aplicação.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // Members
+        private OcrManager _manager;
+
+        // Constructor
+        /// <summary>
+        /// Inicializa componentes e carrega uma instância da classe de entrada da Core.
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Salva uma instância da classe de entrada da Core.
+            this._manager = new OcrManager();
+        }
+
+        // Private methods
+        /// <summary>
+        /// Chama método para carregar imagem.
+        /// </summary>
+        /// <param name="sender">Botão de carregar imagem.</param>
+        /// <param name="e">Argumentos do evento.</param>
+        private void uxButtonInputFile_Click(object sender, RoutedEventArgs e)
+        {
+            this.LoadImage();
+        }
+        /// <summary>
+        /// Método que recupera imagem no storage do equipamento e coloca na tela.
+        /// </summary>
+        private async void LoadImage()
+        {
+            StorageFile file = await this._manager.GetImageFromStorage();
+
+            if (file == null) // Se o usuário não escolheu nenhuma imagem da biblioteca.
+            {
+                return;
+            }
+            else
+            {
+                // Coloca nome e caminho do arquivo no textbox de input file.
+                this.uxTextBoxInputFile.Text = file.Path + file.Name;
+
+                // Abre arquivo carregado.
+                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    // Converte para bitmap.
+                    BitmapImage bitmap = new BitmapImage();
+                    await bitmap.SetSourceAsync(stream);
+                    // Define como source para imagem na tela.
+                    this.uxImageInput.Source = bitmap;
+                }
+            }
         }
     }
 }
